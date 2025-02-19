861daf2213a94eabc7becc4a1c1a2175b0e4445e
Uwe Fechner <fechner@aenarete.eu>
2025-02-19 22:16:56 +0100

    more tests

--- test/test_plotting.jl
+++ test/test_plotting.jl
@@ -4,8 +4,13 @@ using Test
 
 plt.ioff()
 @testset "Plotting" begin
-    fig = plt.plot([1,2,3])
-    @test fig isa Vector{plt.PyObject}
+    fig = plt.figure(figsize=(14, 14))
+    res = plt.plot([1,2,3])
+    @test fig isa plt.PyPlot.Figure
+    @test res isa Vector{plt.PyObject}
+    @test isfile("/tmp/plot.pdf")
+    rm("/tmp/plot.pdf")
     show_plot(fig)
+    save_plot(fig, "/tmp", "plot")
 end
 plt.ion()
