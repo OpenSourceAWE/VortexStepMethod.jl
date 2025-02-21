ed80331f65e7041eba2aa926cfef232a272b639c
Uwe Fechner <fechner@aenarete.eu>
2025-02-21 17:23:28 +0100

    Added menu()

---
+++ examples/menu.jl
@@ -0,0 +1,23 @@
+using REPL.TerminalMenus
+
+options = ["rectangular_wing = include(\"rectangular_wing.jl\")",
+           "ram_air_kite = include(\"ram_air_kite.jl\")",
+           "stall_model = include(\"stall_model.jl\")",
+           "quit"]
+
+function example_menu()
+    active = true
+    while active
+        menu = RadioMenu(options, pagesize=8)
+        choice = request("\nChoose function to execute or `q` to quit: ", menu)
+
+        if choice != -1 && choice != length(options)
+            eval(Meta.parse(options[choice]))
+        else
+            println("Left menu. Press <ctrl><d> to quit Julia!")
+            active = false
+        end
+    end
+end
+
+example_menu()
