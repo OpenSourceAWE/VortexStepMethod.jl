1421cfea5d2d84c18650e7c6f01416fd6a8c88f2
Uwe Fechner <fechner@aenarete.eu>
2025-02-24 16:22:37 +0100

    add bench.jl

---
+++ examples/bench.jl
@@ -0,0 +1,46 @@
+using LinearAlgebra
+using ControlPlots
+using VortexStepMethod
+
+plot = true
+
+# Step 1: Define wing parameters
+n_panels = 20          # Number of panels
+span = 20.0            # Wing span [m]
+chord = 1.0            # Chord length [m]
+v_a = 20.0            # Magnitude of inflow velocity [m/s]
+density = 1.225        # Air density [kg/m³]
+alpha_deg = 30.0       # Angle of attack [degrees]
+alpha = deg2rad(alpha_deg)
+
+# Step 2: Create wing geometry with linear panel distribution
+wing = Wing(n_panels, spanwise_panel_distribution=:linear)
+
+# Add wing sections - defining only tip sections with inviscid airfoil model
+add_section!(wing, 
+    [0.0, span/2, 0.0],   # Left tip LE 
+    [chord, span/2, 0.0],  # Left tip TE
+    :inviscid)
+add_section!(wing, 
+    [0.0, -span/2, 0.0],  # Right tip LE
+    [chord, -span/2, 0.0], # Right tip TE
+    :inviscid)
+
+# Step 3: Initialize aerodynamics
+wa = BodyAerodynamics([wing])
+
+# Set inflow conditions
+vel_app = [cos(alpha), 0.0, sin(alpha)] .* v_a
+set_va!(wa, (vel_app, 0.0))  # Second parameter is yaw rate
+
+# Step 4: Initialize solvers for both LLT and VSM methods
+llt_solver = Solver(aerodynamic_model_type=:LLT)
+vsm_solver = Solver(aerodynamic_model_type=:VSM)
+
+# Step 5: Solve using both methods
+results_vsm = solve(vsm_solver, wa)
+@time results_vsm = solve(vsm_solver, wa)
+# time Python: 32.0 ms
+# time Julia:   0.7 ms
+
+nothing
