2c275c75a16bd003d6e4b2bc64056ec1779fc08e
1-Bart-1 <bart@vandelint.net>
2025-02-26 08:27:02 -0600

    change back to old bench

--- examples/bench.jl
+++ examples/bench.jl
@@ -1,42 +1,47 @@
-using VortexStepMethod
 using LinearAlgebra
-using Pkg
-
-if !("CSV" ∈ keys(Pkg.project().dependencies))
-    using TestEnv
-    TestEnv.activate()
-end
-using CSV
-using DataFrames
-using BenchmarkTools
+using ControlPlots
+using VortexStepMethod
 
 plot = true
 
-# Create wing geometry
-wing = KiteWing("data/ram_air_kite_body.obj", "data/ram_air_kite_foil.dat")
-body_aero = BodyAerodynamics([wing])
+# Step 1: Define wing parameters
+n_panels = 20          # Number of panels
+span = 20.0            # Wing span [m]
+chord = 1.0            # Chord length [m]
+v_a = 20.0            # Magnitude of inflow velocity [m/s]
+density = 1.225        # Air density [kg/m³]
+alpha_deg = 30.0       # Angle of attack [degrees]
+alpha = deg2rad(alpha_deg)
 
-# Create solvers
-VSM = Solver(
-    aerodynamic_model_type=:VSM,
-    is_with_artificial_damping=false
-)
+# Step 2: Create wing geometry with linear panel distribution
+wing = Wing(n_panels, spanwise_panel_distribution=:linear)
 
-# Setting velocity conditions
-v_a = 15.0
-aoa = 15.0
-side_slip = 0.0
-yaw_rate = 0.0
-aoa_rad = deg2rad(aoa)
-vel_app = [
-    cos(aoa_rad) * cos(side_slip),
-    sin(side_slip),
-    sin(aoa_rad)
-] * v_a
-body_aero.va = vel_app
+# Add wing sections - defining only tip sections with inviscid airfoil model
+add_section!(wing, 
+    [0.0, span/2, 0.0],   # Left tip LE 
+    [chord, span/2, 0.0],  # Left tip TE
+    :inviscid)
+add_section!(wing, 
+    [0.0, -span/2, 0.0],  # Right tip LE
+    [chord, -span/2, 0.0], # Right tip TE
+    :inviscid)
 
-# Solving and plotting distributions
-results = solve(VSM, body_aero)
-@btime results = solve($VSM, $body_aero)
+# Step 3: Initialize aerodynamics
+wa = BodyAerodynamics([wing])
+
+# Set inflow conditions
+vel_app = [cos(alpha), 0.0, sin(alpha)] .* v_a
+set_va!(wa, (vel_app, 0.0))  # Second parameter is yaw rate
+
+# Step 4: Initialize solvers for both LLT and VSM methods
+llt_solver = Solver(aerodynamic_model_type=:LLT)
+vsm_solver = Solver(aerodynamic_model_type=:VSM)
+
+# Step 5: Solve using both methods
+results_vsm = solve(vsm_solver, wa)
+@time results_vsm = solve(vsm_solver, wa)
+# time Python: 32.0 ms Ryzen 7950x
+# time Julia:   0.6 ms Ryzen 7950x
+#               0.8 ms laptop, performance mode, battery 
 
 nothing
