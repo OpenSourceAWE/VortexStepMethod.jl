10b8f5a9a5a0088a7ff403d708ea3d0b62ad022b
Uwe Fechner <fechner@aenarete.eu>
2025-02-26 08:59:13 +0100

    fix example

--- examples/ram_air_kite.jl
+++ examples/ram_air_kite.jl
@@ -57,7 +57,7 @@ results = solve(VSM, body_aero)
 body_y_coordinates = [panel.aero_center[2] for panel in body_aero.panels]
 
 plot && plot_distribution(
-    [CAD_y_coordinates],
+    [body_y_coordinates],
     [results],
     ["VSM"];
     title="CAD_spanwise_distributions_alpha_$(round(aoa, digits=1))_beta_$(round(side_slip, digits=1))_yaw_$(round(yaw_rate, digits=1))_v_a_$(round(v_a, digits=1))",
