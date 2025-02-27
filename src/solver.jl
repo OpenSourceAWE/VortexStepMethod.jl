0025c2e833320207695183f98b439b49cec005ce
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-02-27 23:21:08 +0100

    Add unit.

--- src/solver.jl
+++ src/solver.jl
@@ -83,7 +83,7 @@ Main solving routine for the aerodynamic model. Reference point is in the kite b
 # Arguments:
 - solver::Solver: The solver to use, could be a VSM or LLT solver. See: [Solver](@ref)
 - body_aero::BodyAerodynamics: The aerodynamic body. See: [BodyAerodynamics](@ref)
-- gamma_distribution: Initial circulation vector or nothing; Length: Number of segments.
+- gamma_distribution: Initial circulation vector or nothing; Length: Number of segments. [m²/s]
 
 # Keyword Arguments:
 - log=false: If true, print the number of iterations and other info.
