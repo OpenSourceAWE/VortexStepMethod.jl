f2f129a19dfbe22d10e6ce52a0138fc29540ec62
Uwe Fechner <fechner@aenarete.eu>
2025-02-28 19:38:26 +0100

    Fix docstrings

--- src/VortexStepMethod.jl
+++ src/VortexStepMethod.jl
@@ -47,14 +47,14 @@ Basic 3-dimensional vector, stack allocated, mutable.
 const MVec3    = MVector{3, Float64}
 
 """
-   const PosVector=Union{MVec3, Vector, SizedVector{3, Float64, Vector{Float64}}}
+   const PosVector=Union{MVec3, Vector}
 
 Position vector, either a `MVec3` or a `Vector` for use in function signatures.
 """
 const PosVector=Union{MVec3, Vector}
 
 """
-   const VelVector=Union{MVec3, Vector, SizedVector{3, Float64, Vector{Float64}}}
+   const VelVector=Union{MVec3, Vector}
 
 Velocity vector, either a `MVec3` or a `Vector` for use in function signatures.
 """
