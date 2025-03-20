c56e1746e3cd46f245921b0e689b0a3b18af807f
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-03-20 07:11:32 +0100

    Update comment

--- src/solver.jl
+++ src/solver.jl
@@ -220,7 +220,7 @@ function solve!(solver::Solver, body_aero::BodyAerodynamics, gamma_distribution=
     # Calculate wing geometry properties
     projected_area = body_aero.projected_area
     
-    for (i, panel) in enumerate(panels)                                               # 8008 bytes
+    for (i, panel) in enumerate(panels)                                               # 8000 bytes
 
         ### Lift and Drag ###
         # Panel geometry
