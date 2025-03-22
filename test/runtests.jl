c12e412aaf769c76054d8931980ac0a2d087f29a
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-03-22 04:24:15 +0100

    First fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,21 @@
 using Test
 
 # TODO fix allocation tests
-# see: https://github.com/JuliaArrays/FixedSizeArrays.jl/blob/d17373edf4144a672cd80a062bf24d017f01e42f/test/runtests.jl#L6-L17
 # and: https://github.com/JuliaArrays/FixedSizeArrays.jl/blob/d17373edf4144a672cd80a062bf24d017f01e42f/.github/workflows/UnitTests.yml
+
+# Check if the compilation options allow maximum performance.
+const build_is_production_build_env_name = "BUILD_IS_PRODUCTION_BUILD"
+const build_is_production_build = let v = get(ENV, build_is_production_build_env_name, "true")
+    if v ∉ ("false", "true")
+        error("unknown value for environment variable $build_is_production_build_env_name: $v")
+    end
+    if v == "true"
+        true
+    else
+        false
+    end
+end::Bool
+
 cd("..")
 println("Running tests...")
 @testset verbose = true "Testing VortexStepMethod..." begin
