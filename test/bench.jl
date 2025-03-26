26dd4e0a53805cb7501af5891ef0805bfcd083d8
Uwe Fechner <uwe.fechner.msc@gmail.com>
2025-03-26 17:08:56 +0100

    Refactoring: add underscore to private fields (#142)
    * Refactoring (make private fields private)
    
    * Refactoring
    
    * Refactoring
    
    * Refactoring
    
    * Refactoring
    
    * Refactoring
    
    ---------
    
    Co-authored-by: Uwe Fechner <u.fechner-1@tudelft.nl>

--- test/bench.jl
+++ test/bench.jl
@@ -128,11 +128,11 @@ using LinearAlgebra
                 solver = Solver{P}(
                     aerodynamic_model_type=model
                 )
-                solver.sol.va_array .= va_array
-                solver.sol.chord_array .= chord_array
-                solver.sol.x_airf_array .= x_airf_array
-                solver.sol.y_airf_array .= y_airf_array
-                solver.sol.z_airf_array .= z_airf_array
+                solver.sol._va_array .= va_array
+                solver.sol._chord_array .= chord_array
+                solver.sol._x_airf_array .= x_airf_array
+                solver.sol._y_airf_array .= y_airf_array
+                solver.sol._z_airf_array .= z_airf_array
                 result = @benchmark gamma_loop!(
                     $solver,
                     $body_aero,
