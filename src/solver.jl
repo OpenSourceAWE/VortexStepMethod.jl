456d7b610d6939946e88fef6fba90e63fdb6fd57
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-03-26 15:30:42 +0100

    Update docstring of VSMSolution

--- src/solver.jl
+++ src/solver.jl
@@ -5,6 +5,15 @@
 Struct for storing the solution of the [solve!](@ref) function. Must contain all info needed by `KiteModels.jl`.
 
 # Attributes
+- `panel_width_array`::Vector{Float64}: Width of the panels [m]
+- cl_array::Vector{Float64}: Lift coefficients of the panels [-]
+- cd_array::Vector{Float64}: Drag coefficients of the panels [-]
+- cm_array::Vector{Float64}: Pitching moment coefficients of the panels [-]
+- panel_lift::Matrix{Float64}: Lift force of the panels [N]
+- panel_drag::Matrix{Float64}: Drag force of the panels [N]
+- panel_moment::Matrix{Float64}: Pitching moment around the spanwise vector of the panels [Nm]
+- `f_body_3D`::Matrix{Float64}: Matrix of the aerodynamic forces (x, y, z vectors) [N]
+- `m_body_3D`::Matrix{Float64}: Matrix of the aerodynamic moments [Nm]
 - `gamma_distribution`::Union{Nothing, Vector{Float64}}: Vector containing the panel circulations.
 - force::MVec3: Aerodynamic force vector in KB reference frame [N]
 - moment::MVec3: Aerodynamic moments [Mx, My, Mz] around the reference point [Nm]
