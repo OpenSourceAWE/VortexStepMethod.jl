545a16c0f3f9c5709fc33f758180ece16ae642ce
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-04-02 14:25:09 +0200

    WingSettings defined correctly

--- mwes/mwe_04.jl
+++ mwes/mwe_04.jl
@@ -1,16 +1,19 @@
 # read the vsm_settings.yaml file
 
 import YAML
-using Parameters
+using Parameters, StaticArrays
+
+const MVec3    = MVector{3, Float64}
 
 filename = "vsm_settings.yaml"
 data = YAML.load_file(joinpath("data", filename))
 
 @with_kw mutable struct WingSettings
     name::String = "main_wing"
-    n_groups::Int64 = 40 
     n_panels::Int64 = 40
+    n_groups::Int64 = 40 
     spanwise_panel_distribution::String = "LINEAR"
+    spanwise_direction::MVec3 = [0.0, 1.0, 0.0]
 end
 
 @with_kw mutable struct SolverSettings
