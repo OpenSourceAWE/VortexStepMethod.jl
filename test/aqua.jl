a654e91fed9934abffeda36497d5323dc1f95448
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-04-17 17:00:36 +0200

    Next try

--- test/aqua.jl
+++ test/aqua.jl
@@ -2,7 +2,7 @@ using Aqua
 @testset "Aqua.jl" begin
     Aqua.test_all(
       VortexStepMethod;
-      stale_deps=(ignore=[:Xfoil, :Timers],),
+      stale_deps=(ignore=[:Xfoil, :Timers, :PyCall],),
       deps_compat=(ignore=[:PyCall],)
     )
   end
