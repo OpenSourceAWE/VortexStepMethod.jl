85649da3e63032ae6881cdb164f14ab14ecc23f7
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-08-13 21:00:26 +0200

    Add stats.jl script

---
+++ scripts/stats.jl
@@ -0,0 +1,36 @@
+# Copyright (c) 2025 Uwe Fechner
+# SPDX-License-Identifier: BSD-3-Clause
+
+# List all methods of VortexStepMethod
+using VortexStepMethod  
+
+exported_names = names(VortexStepMethod)
+exported_functions = filter(n -> isdefined(VortexStepMethod, n) && isa(getfield(VortexStepMethod, n), Function), exported_names)
+println("Exported methods:\n")
+total = 0
+for fun in exported_functions
+    global total
+    f = getfield(VortexStepMethod, fun)
+    mes = methods(f)
+    for me in mes
+        println(split(repr(me),'@')[1])
+        total += 1
+    end
+end
+println("\nTotal: $total")
+
+function check_exported_docs(mod::Module)
+    exported_symbols = names(mod, all=false)
+    doc_status = Dict{Symbol,Bool}()
+    for sym in exported_symbols
+        #if isa(getfield(mod, sym), Function)
+            doc_status[sym] = Base.Docs.hasdoc(mod, sym)
+        #end
+    end
+    return doc_status
+end
+
+# Usage example:
+results = check_exported_docs(VortexStepMethod)
+undocumented = filter(kv -> !kv[2], results)
+println("\nUndocumented exported symbols: \n", keys(undocumented))
