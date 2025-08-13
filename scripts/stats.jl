52ce16d356ea7cb6daccce6727f11ee692d38cdb
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-08-13 21:16:47 +0200

    Refactoring

--- scripts/stats.jl
+++ scripts/stats.jl
@@ -1,31 +1,38 @@
 # Copyright (c) 2025 Uwe Fechner
 # SPDX-License-Identifier: BSD-3-Clause
 
-# List all methods of VortexStepMethod
-using VortexStepMethod  
+# List all methods of VortexStepMethod and report if they are documented;
+# By default, also exported constants are reported
+using VortexStepMethod
 
 exported_names = names(VortexStepMethod)
 exported_functions = filter(n -> isdefined(VortexStepMethod, n) && isa(getfield(VortexStepMethod, n), Function), exported_names)
-println("Exported methods:\n")
-total = 0
-for fun in exported_functions
-    global total
-    f = getfield(VortexStepMethod, fun)
-    mes = methods(f)
-    for me in mes
-        println(split(repr(me),'@')[1])
-        total += 1
+
+function find_exported_functions(exported_functions)
+    total = 0
+    for fun in exported_functions
+        f = getfield(VortexStepMethod, fun)
+        mes = methods(f)
+        for me in mes
+            println(split(repr(me),'@')[1])
+            total += 1
+        end
     end
+    return total
 end
+
+println("Exported methods:\n")
+total = find_exported_functions(exported_functions)
 println("\nTotal: $total")
 
-function check_exported_docs(mod::Module)
+function check_exported_docs(mod::Module; only_functions=false)
     exported_symbols = names(mod, all=false)
     doc_status = Dict{Symbol,Bool}()
     for sym in exported_symbols
-        #if isa(getfield(mod, sym), Function)
-            doc_status[sym] = Base.Docs.hasdoc(mod, sym)
-        #end
+        if only_functions && !isa(getfield(mod, sym), Function)
+            continue
+        end
+        doc_status[sym] = Base.Docs.hasdoc(mod, sym)
     end
     return doc_status
 end
