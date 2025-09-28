86f08571d4c270616acf04e85dd1c54cd49b58b0
1-Bart-1 <bart@vandelint.net>
2025-09-28 16:50:49 +0200

    No specific data type

--- ext/VortexStepMethodMakieExt.jl
+++ ext/VortexStepMethodMakieExt.jl
@@ -2,13 +2,13 @@ module VortexStepMethodMakieExt
 using Makie, VortexStepMethod
 
 """
-    plot!(ax::Makie.Axis3, panel::VortexStepMethod.Panel; kwargs...)
+    plot!(ax, panel::VortexStepMethod.Panel; kwargs...)
 
 Plot a single `Panel` as a `mesh`.
 The corner points are ordered as: LE1, TE1, TE2, LE2.
 This creates two triangles: (LE1, TE1, TE2) and (LE1, TE2, LE2).
 """
-function Makie.plot!(ax::Makie.Axis3, panel::VortexStepMethod.Panel; color=(:red, 0.2), R_b_w=nothing, T_b_w=nothing, kwargs...)
+function Makie.plot!(ax, panel::VortexStepMethod.Panel; color=(:red, 0.2), R_b_w=nothing, T_b_w=nothing, kwargs...)
     points = [Point3f(panel.corner_points[:, i]) for i in 1:4]
     if !isnothing(R_b_w) && !isnothing(T_b_w)
         points = [Point3f(R_b_w * p + T_b_w) for p in points]
@@ -20,11 +20,11 @@ function Makie.plot!(ax::Makie.Axis3, panel::VortexStepMethod.Panel; color=(:red
 end
 
 """
-    plot!(ax::Makie.Axis3, body::VortexStepMethod.BodyAerodynamics; kwargs...)
+    plot!(ax, body::VortexStepMethod.BodyAerodynamics; kwargs...)
 
 Plot a `BodyAerodynamics` object by plotting each of its panels.
 """
-function Makie.plot!(ax::Makie.Axis3, body::VortexStepMethod.BodyAerodynamics; color=(:red, 0.2), R_b_w=nothing, T_b_w=nothing, kwargs...)
+function Makie.plot!(ax, body::VortexStepMethod.BodyAerodynamics; color=(:red, 0.2), R_b_w=nothing, T_b_w=nothing, kwargs...)
     for panel in body.panels
         Makie.plot!(ax, panel; color, R_b_w, T_b_w, kwargs...)
     end
