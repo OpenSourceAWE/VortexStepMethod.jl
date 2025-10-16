081566f885d8cd9b6db5654a1cd6ddf094685e94
Bart van de Lint <33758964+1-Bart-1@users.noreply.github.com>
2025-10-16 10:44:59 +0200

    Add Makie plotting extension (#199)
    * Add plotting methods
    
    * Plot panels
    
    * Plot body aero
    
    * Add transforms
    
    * Rebase example
    
    * No specific data type
    
    * Return plots
    
    * Remove white stuff

---
+++ ext/VortexStepMethodMakieExt.jl
@@ -0,0 +1,65 @@
+module VortexStepMethodMakieExt
+using Makie, VortexStepMethod
+
+"""
+    plot!(ax, panel::VortexStepMethod.Panel; kwargs...)
+
+Plot a single `Panel` as a `mesh`.
+The corner points are ordered as: LE1, TE1, TE2, LE2.
+This creates two triangles: (LE1, TE1, TE2) and (LE1, TE2, LE2).
+"""
+function Makie.plot!(ax, panel::VortexStepMethod.Panel; color=(:red, 0.2), R_b_w=nothing, T_b_w=nothing, kwargs...)
+    plots = []
+    points = [Point3f(panel.corner_points[:, i]) for i in 1:4]
+    if !isnothing(R_b_w) && !isnothing(T_b_w)
+        points = [Point3f(R_b_w * p + T_b_w) for p in points]
+    end
+    faces = [Makie.GLTriangleFace(1, 2, 3), Makie.GLTriangleFace(1, 3, 4)]
+    p = mesh!(ax, points, faces; color, transparency=true, kwargs...)
+    push!(plots, p)
+    border_points = [points..., points[1]]
+    p = lines!(ax, border_points; color=:black, transparency=true, kwargs...)
+    push!(plots, p)
+    return plots
+end
+
+"""
+    plot!(ax, body::VortexStepMethod.BodyAerodynamics; kwargs...)
+
+Plot a `BodyAerodynamics` object by plotting each of its panels.
+"""
+function Makie.plot!(ax, body::VortexStepMethod.BodyAerodynamics; color=(:red, 0.2), R_b_w=nothing, T_b_w=nothing, kwargs...)
+    plots = []
+    for panel in body.panels
+        p = Makie.plot!(ax, panel; color, R_b_w, T_b_w, kwargs...)
+        push!(plots, p)
+    end
+    return plots
+end
+
+function Makie.plot(panel::VortexStepMethod.Panel; size = (1200, 800), kwargs...)
+    fig = Figure(; size)
+    ax = Axis3(fig[1, 1]; aspect = :data,
+               xlabel = "X", ylabel = "Y", zlabel = "Z",
+               azimuth = 9/8*π, zoommode = :cursor, viewmode = :fit,
+           )
+
+    plot!(ax, panel; kwargs...)
+    return fig
+end
+
+function Makie.plot(body_aero::VortexStepMethod.BodyAerodynamics; size = (1200, 800),
+                    limitmargin = 0.1, kwargs...)
+    fig = Figure(; size)
+    ax = Axis3(fig[1, 1]; aspect = :data,
+               xlabel = "X", ylabel = "Y", zlabel = "Z",
+               azimuth = 9/8*π, zoommode = :cursor, viewmode = :fit,
+               xautolimitmargin=(limitmargin, limitmargin),
+               yautolimitmargin=(limitmargin, limitmargin),
+               zautolimitmargin=(limitmargin, limitmargin),
+           )
+    plot!(ax, body_aero; kwargs...)
+    return fig
+end
+
+end
