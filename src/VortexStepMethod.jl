1ddc738b17e7313a0e5e256cdc955c3f5a124426
Bart <bart@vandelint.net>
2026-02-12 13:05:17 +0100

    Simplify install examples

--- src/VortexStepMethod.jl
+++ src/VortexStepMethod.jl
@@ -227,21 +227,14 @@ end
 
 function install_examples(add_packages=true)
     copy_examples()
+    pkg_root = joinpath(dirname(pathof(@__MODULE__)), "..")
+    src = joinpath(pkg_root, "data")
+    isdir(src) && cp(src, "data"; force=true)
     if add_packages
-        if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
-            Pkg.add("ControlPlots")
-        end
-        if ! ("LaTeXStrings" ∈ keys(Pkg.project().dependencies))
-            Pkg.add("LaTeXStrings")
-        end
-        if ! ("Xfoil" ∈ keys(Pkg.project().dependencies))
-            Pkg.add("Xfoil")
-        end
-        if ! ("CSV" ∈ keys(Pkg.project().dependencies))
-            Pkg.add("CSV")
-        end
-        if ! ("DataFrames" ∈ keys(Pkg.project().dependencies))
-            Pkg.add("DataFrames")
+        for pkg in ("GLMakie", "LaTeXStrings", "Xfoil", "CSV", "DataFrames")
+            if !(pkg ∈ keys(Pkg.project().dependencies))
+                Pkg.add(pkg)
+            end
         end
     end
 end
