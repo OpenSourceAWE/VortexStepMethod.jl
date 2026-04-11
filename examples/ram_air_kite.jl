cfe5a53ea601ed94ed321f1e2cc2a596fd902251
Uwe Fechner <fechner@aenarete.eu>
2026-04-11 14:36:35 +0200

    Fix warning

--- examples/ram_air_kite.jl
+++ examples/ram_air_kite.jl
@@ -72,13 +72,12 @@ end
 
 # Solving
 println("Solve")
-results = nothing
 results = VortexStepMethod.solve(solver, body_aero; log=true)
-@time results = VortexStepMethod.solve(solver, body_aero; log=true)
+@time VortexStepMethod.solve(solver, body_aero; log=true)
 
 body_y_coordinates = [panel.aero_center[2] for panel in body_aero.panels]
 
-if PLOT && !isnothing(results)
+if PLOT
     plot_combined_analysis(
         solver,
         body_aero,
