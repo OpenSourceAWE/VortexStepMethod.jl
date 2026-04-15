a7fa1150d4de671a7d001d15c792c8034c82a8e7
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-04-15 16:38:51 +0200

    Add menu_cp.jl

---
+++ examples_cp/menu_cp.jl
@@ -0,0 +1,33 @@
+using Pkg
+
+using REPL.TerminalMenus
+
+url = "https://opensourceawe.github.io/VortexStepMethod.jl/dev"
+
+options = [
+           "V3_kite = include(\"V3_kite.jl\")",
+           "pyramid_model = include(\"pyramid_model.jl\")",
+           "rectangular_wing = include(\"rectangular_wing.jl\")",
+           "ram_air_kite = include(\"ram_air_kite.jl\")",
+           "stall_model = include(\"stall_model.jl\")",
+           "bench = include(\"bench.jl\")",
+           "cleanup = include(\"cleanup.jl\")",
+           "help_me = VortexStepMethod.help(url)",
+           "quit"]
+
+function example_menu()
+    active = true
+    while active
+        menu = RadioMenu(options, pagesize=8)
+        choice = request("\nChoose function to execute or `q` to quit: ", menu)
+
+        if choice != -1 && choice != length(options)
+            eval(Meta.parse(options[choice]))
+        else
+            println("Left menu. Press <ctrl><d> to quit Julia!")
+            active = false
+        end
+    end
+end
+
+example_menu()
