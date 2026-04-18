5dafe5d07f1e75e57e756c19a68ea0d05d5d32d9
Uwe Fechner <fechner@aenarete.eu>
2026-04-18 19:09:38 +0200

    Fix build_docu.jl

--- scripts/build_docu.jl
+++ scripts/build_docu.jl
@@ -2,6 +2,5 @@
 # run with: julia --project=docs scripts/build_docu.jl
 
 using Pkg
-Pkg.develop(path=dirname(@__DIR__))
-Pkg.instantiate()
+Pkg.activate(joinpath(@__DIR__, "..", "docs"))
 using LiveServer; servedocs(launch_browser=true)
